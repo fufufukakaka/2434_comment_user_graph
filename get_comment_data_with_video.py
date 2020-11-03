ec00198e47bbbeeed25a98cfaccf1b0f73c38c78
Yusuke Fukasawa <yusuke-fukasawa@cookpad.com>
2020-11-03 20:21:43 +0900

    get comment data script

--- get_comment_data_with_video.py
+++ get_comment_data_with_video.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import requests
 import os
 from pytchat import Extractor, VideoInfo, TSVArchiver
 from pytchat.exceptions import InvalidVideoIdException
@@ -12,35 +11,49 @@ def disp(*args):
 
 
 def main():
-    video_id = "eu90PK3bpuw"
-    save_path = "test.txt"
-    archiver = TSVArchiver(save_path)
-
-    try:
-        # Extractorの生成
-        ex = Extractor(video_id, div=10, callback=disp, processor=archiver)
-    except InvalidVideoIdException as e:
-        print(e)
-        exit(0)
-
-    # Ctrl+Cでキャンセルする
-    signal.signal(signal.SIGINT, (lambda a, b: ex.cancel()))
-
-    # 抽出の開始
-    info = VideoInfo(video_id)
-    if info.get_duration() == 0:
-        print("指定した動画はアーカイブされていないか、チャットデータが存在しません")
-        exit(0)
-    print("Extracting: " + info.get_title())
-    print(
-        "Save path: ",
-        os.path.realpath(save_path)[: -len(os.path.basename(save_path))]
-        + os.path.basename(archiver.save_path),
+    all_video_list = pd.read_csv("concat_video_list.csv")
+    video_author_list = (
+        all_video_list.groupby(["streamer_name"])["videoId"].apply(list).reset_index()
     )
-    result = ex.extract()
+    video_author_list["video_num"] = video_author_list["videoId"].apply(len)
+    video_author_list = video_author_list.query("video_num > 30")
+    video_author_list["use_videos"] = video_author_list["videoId"].apply(
+        lambda x: x[:30]
+    )
+    streamers = video_author_list["streamer_name"].unique()
+    for s in streamers[0:]:
+        video_list = video_author_list.query(f"streamer_name == @s")[
+            "use_videos"
+        ].values[0]
+        for video_id in video_list:
+            save_path = f"comment_list_output/{video_id}.csv"
+            archiver = TSVArchiver(save_path)
 
-    # 抽出完了
-    print("\nFinished extraction.")
+            try:
+                # Extractorの生成
+                ex = Extractor(video_id, div=10, callback=disp, processor=archiver)
+            except InvalidVideoIdException as e:
+                print(e)
+                exit(0)
+
+            # Ctrl+Cでキャンセルする
+            signal.signal(signal.SIGINT, (lambda a, b: ex.cancel()))
+
+            # 抽出の開始
+            info = VideoInfo(video_id)
+            if info.get_duration() == 0:
+                print("指定した動画はアーカイブされていないか、チャットデータが存在しません")
+                exit(0)
+            print("Extracting: " + info.get_title())
+            print(
+                "Save path: ",
+                os.path.realpath(save_path)[: -len(os.path.basename(save_path))]
+                + os.path.basename(archiver.save_path),
+            )
+            result = ex.extract()
+
+            # 抽出完了
+            print("...Finised")
 
 
 if __name__ == "__main__":
