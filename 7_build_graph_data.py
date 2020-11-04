e4007258afee003a3a4ce65d95108aa663cf9e32
yusuke-fukasawa <yusuke-fukasawa@cookpad.com>
2020-11-04 23:41:24 +0000

    setにして早くなった

--- 7_build_graph_data.py
+++ 7_build_graph_data.py
@@ -4,20 +4,14 @@ import pandas as pd
 from tqdm.auto import tqdm
 
 
-def get_edge(streamers, all_chat_users, chat_user):
-    _edge_list = []
+def get_combination(streamers, all_chat_users, chat_user):
     _chated_streamers = []
     for streamer, chat_users in zip(streamers, all_chat_users):
         if chat_user in chat_users:
             _chated_streamers.append(streamer)
     # コメントしたstreamerのリストをcombinationして組み合わせをすべて出す
     combinations = list(itertools.combinations(_chated_streamers, 2))
-    # networkxで読める形式に出力する
-    for combi in combinations:
-        # source, target, edge_name
-        edge = [combi[0], combi[1], chat_user]
-        _edge_list.append(edge)
-    return _edge_list
+    return combinations
 
 
 def main():
@@ -34,12 +28,16 @@ def main():
     ].to_dict()
 
     streamers = list(streamer2chat_users.keys())
-    all_chat_users = list(streamer2chat_users.values())
-    _edge_list = []
+    all_chat_users = [set(v) for v in streamer2chat_users.values()]
+    edge_list = []
     for chat_user in tqdm(unique_chat_users):
-        edges = get_edge(streamers, all_chat_users, chat_user)
-        _edge_list.append(edges)
-    edge_list = sum(_edge_list, [])
+        combinations = get_combination(streamers, all_chat_users, chat_user)
+        # networkxで読める形式に出力する
+        for combi in combinations:
+            # source, target, edge_name
+            edge = [combi[0], combi[1], chat_user]
+            edge_list.append(edge)
+
     edge_df = pd.DataFrame(edge_list, columns=["source", "target", "edge_name"])
     edge_df.to_csv("streamer_chat_user_edge_df.csv", index=None)
 
